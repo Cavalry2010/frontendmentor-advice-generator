31758736927f15d475fdbe3c73031d5920fdf7b6
Cavalry2010 <el.cavalry.2010@gmail.com>
2023-04-17 00:53:26 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,53 @@
+# Frontend Mentor - Advice generator app solution
+
+This is a solution to the [Advice generator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/advice-generator-app-QdUG-13db). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the app depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Generate a new piece of advice by clicking the dice icon
+
+### Screenshot
+
+![Desktop Preview](.//images/screenshot.png)
+
+### Links
+
+- Solution URL: [Frontend Mentor Solution](https://your-solution-url.com)
+- Live Site URL: [Advice Generator App](https://advice-generator-alejandro.netlify.app)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Desktop-first workflow
+- JavaScript (ES6)
+
+### What I learned
+
+This challenge helped me learn more about promises and API calling.
+
+## Author
+
+- Frontend Mentor - [@Cavalry2010](https://www.frontendmentor.io/profile/Cavalry2010)
+- Twitter - [@AlPerdomoMC](https://www.twitter.com/AlPerdomoMC)
+- GitHub - [@Cavalry2010](https://www.github.com/Cavalry2010)
---
+++ js/script.js
@@ -0,0 +1,78 @@
+"use strict";
+
+class AdviceApp {
+  adviceBox = document.querySelector(".advice-generator-box");
+  adviceId = document.querySelector(".advice-num");
+  adviceText = document.querySelector(".advice-text");
+  getAdviceBtn = document.querySelector(".get-advice-btn");
+
+  constructor() {
+    // this.getAdvice();
+    this.getAdviceBtn.addEventListener("click", this.getAdvice.bind(this));
+  }
+
+  async getAdvice() {
+    this.adviceBox.classList.add("hide-text");
+    this.getAdviceBtn.disabled = true;
+
+    try {
+      const fetchPro = fetch("https://api.adviceslip.com/advice");
+      const response = await Promise.race([fetchPro, this.timeout(10)]);
+      const data = await response.json();
+
+      if (!response.ok) {
+        throw new Error(`${data.message} (${res.status})`);
+      }
+      this.renderAdvice(data.slip);
+    } catch (error) {
+      console.error(error);
+      this.renderError(error.message);
+    }
+  }
+
+  renderAdvice(slip) {
+    setTimeout(
+      function () {
+        this.adviceId.textContent = slip.id;
+        this.adviceText.innerHTML = `&ldquo;${slip.advice}&rdquo;`;
+      }.bind(this),
+      500
+    );
+
+    setTimeout(
+      function () {
+        this.adviceBox.classList.remove("hide-text");
+        this.getAdviceBtn.disabled = false;
+      }.bind(this),
+      1000
+    );
+  }
+
+  renderError(message) {
+    setTimeout(
+      function () {
+        this.adviceId.textContent = "???";
+        this.adviceText.textContent = `Error: ${message}. Please try again`;
+      }.bind(this),
+      500
+    );
+
+    setTimeout(
+      function () {
+        this.adviceBox.classList.remove("hide-text");
+        this.getAdviceBtn.disabled = false;
+      }.bind(this),
+      1000
+    );
+  }
+
+  timeout(s) {
+    return new Promise(function (_, reject) {
+      setTimeout(function () {
+        reject(new Error(`Request took too long! Timeout after ${s} seconds`));
+      }, s * 1000);
+    });
+  }
+}
+
+const app = new AdviceApp();
